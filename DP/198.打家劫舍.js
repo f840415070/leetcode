40c3148b824d0827ece1ba142e2d21fd230540af
fanyibin <f84041507@163.com>
2021-03-01 18:21:52 +0800

    feat: update

---
+++ DP/198.打家劫舍.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  const dp = [];
+  for (let i = 0; i < nums.length; i++) {
+    
+  }
+};
+// @lc code=end
+
--- DP/474.一和零.js
+++ DP/474.一和零.js
@@ -12,7 +12,31 @@
  * @return {number}
  */
 var findMaxForm = function(strs, m, n) {
-
+  const count_0_1 = (str) => {
+    let c0 = 0;
+    let c1 = 0;
+    for (let i = 0; i < str.length; i++) {
+      str[i] === '0' ? c0++ : c1++;
+    }
+    return [c0, c1];
+  };
+  const dp = [];
+  for (let i = 0; i <= m; i++) {
+    dp[i] = [];
+    for (let j = 0; j <= n; j++) {
+      dp[i][j] = [];
+      for (let k = 0; k < strs.length; k++) {
+        const [c0, c1] = count_0_1(strs[k]);
+        dp[i][j][k] = Math.max(
+          dp[i][j][k - 1] || 0,
+          i >= c0 && j >= c1
+            ? 1 + (dp[i - c0][j - c1][k - 1] || 0)
+            : 0
+        );
+      }
+    }
+  }
+  return dp[m][n][strs.length - 1];
 };
 // @lc code=end
 
