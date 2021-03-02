e6182a9c4e61f1bdf8898a71ea257c57105c5ee4
fanyibin <f84041507@163.com>
2021-03-02 17:57:57 +0800

    feat: DP

---
+++ DP/53.最大子序和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  const dp = [nums[0]];
+  let maxSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    if (dp[i] > maxSum) {
+      maxSum = dp[i];
+    }
+  }
+  return maxSum;
+};
+// @lc code=end
+
