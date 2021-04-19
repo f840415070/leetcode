824eaa5dbd8bd69890740bcb4371c289b3d9f1d0
fanyibin <f84041507@163.com>
2021-04-20 00:05:28 +0800

    feat: update

--- DP/416.分割等和子集.js
+++ DP/416.分割等和子集.js
@@ -14,17 +14,37 @@ var canPartition = function(nums) {
   if (length < 2) {
     return false;
   }
-  const sum = nums.reduce((s, n) => s + n);
+  let sum = 0;
+  let max = nums[0];
+  for (let i = 0; i < length; i++) {
+    sum += nums[i];
+    if (nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  if (sum % 2 !== 0) {
+    return false;
+  }
   const target = sum / 2;
-  if (target % 2 !== 0) {
+  if (max > target) {
     return false;
   }
 
-  const dp = new Array(length).fill(
-    new Array(target + 1).fill(false)
-  );
-  
-
+  const dp = Array(length).fill(Array(target + 1));
+  for (let i = 0; i < length; i++) {
+    dp[i][0] = true;
+  }
+  dp[0][nums[0]] = true;
+  for (let i = 1; i < length; i++) {
+    for (let j = 1; j <= target; j++) {
+      if (nums[i] <= j) {
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+  return dp[length - 1][target];
 };
 // @lc code=end
 
--- test.js
+++ test.js
@@ -1,16 +1,38 @@
-const triangle = [[2],[3,4],[6,5,7],[4,1,8,3]];
 
-var minimumTotal = function(triangle) {
-  const tl = triangle.length;
-  for (let i = tl - 2; i >= 0; i--) {
-    for (let j = 0; j < triangle[i].length; j++) {
-      triangle[i][j] += Math.min(
-        triangle[i + 1][j],
-        triangle[i + 1][j + 1],
-      );
+var canPartition = function(nums) {
+  const { length } = nums;
+  if (length < 2) {
+    return false;
+  }
+  const sum = nums.reduce((s, n) => s + n);
+  const target = sum / 2;
+  if (sum % 2 !== 0) {
+    return false;
+  }
+
+  const dp = new Array(length).fill(
+    new Array(target + 1).fill(false)
+  );
+  for (let i = 0; i < length; i++) {
+    dp[i][0] = true;
+  }
+  dp[0][nums[0]] = true;
+  for (let i = 1; i < length; i++) {
+    for (let j = 1; j <= target; j++) {
+      if (nums[i] <= j) {
+        if (i === 1 && j === 4) {
+          console.log(dp[i][j], dp[0][4])
+        }
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+        if (i === 1 && j === 4) {
+          console.log(dp[i][j], dp[0][4])
+        }
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
     }
   }
-  return triangle[0][0];
+  return dp[length - 1][target];
 };
 
-minimumTotal(triangle);
+canPartition([2, 2, 3, 5]);
