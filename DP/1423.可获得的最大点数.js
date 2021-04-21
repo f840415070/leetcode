f8eafd5bb6474f4228f5bfbae3c5a374ff2f1950
fanyibin <f84041507@163.com>
2021-04-21 17:48:59 +0800

    feat: update

--- DP/1423.可获得的最大点数.js
+++ DP/1423.可获得的最大点数.js
@@ -11,7 +11,24 @@
  * @return {number}
  */
 var maxScore = function(cardPoints, k) {
-
+  const { length } = cardPoints;
+  if (k === length) {
+    return cardPoints.reduce((s, n) => s + n);
+  }
+  const dpHeads = Array(k + 1);
+  const dpTails = Array(k + 1);
+  dpHeads[0] = 0;
+  dpTails[0] = 0;
+  for (let i = 1; i <= k; i++) {
+    dpHeads[i] = dpHeads[i - 1] + cardPoints[i - 1];
+    dpTails[i] = dpTails[i - 1] + cardPoints[length - i];
+  }
+  let result = 0;
+  for (let j = 0; j <= k; j++) {
+    const current = dpHeads[j] + dpTails[k - j];
+    result = current > result ? current : result;
+  }
+  return result;
 };
 // @lc code=end
 
--- test.js
+++ test.js
@@ -1,81 +1,25 @@
 
-var canPartition = function(nums) {
-  const { length } = nums;
-  if (length < 2) {
-    return false;
+var maxScore = function(cardPoints, k) {
+  const { length } = cardPoints;
+  if (k === length) {
+    return cardPoints.reduce((s, n) => s + n);
   }
-  let sum = 0;
-  let max = nums[0];
-  for (let i = 0; i < length; i++) {
-    sum += nums[i];
-    if (nums[i] > max) {
-      max = nums[i];
-    }
+  const dpHeads = Array(k + 1);
+  const dpTails = Array(k + 1);
+  dpHeads[0] = 0;
+  dpTails[0] = 0;
+  for (let i = 1; i <= k; i++) {
+    dpHeads[i] = dpHeads[i - 1] + cardPoints[i - 1];
+    dpTails[i] = dpTails[i - 1] + cardPoints[length - i];
   }
-  if (sum % 2 !== 0) {
-    return false;
+  console.log(dpHeads);
+  console.log(dpTails);
+  let result = 0;
+  for (let j = 0; j <= k; j++) {
+    const current = dpHeads[j] + dpTails[k - j];
+    result = current > result ? current : result;
   }
-  const target = sum / 2;
-  if (max > target) {
-    return false;
-  }
-
-  const dp = new Array(length).fill(new Array(target + 1).fill(false));
-  const dp = new Array(length).fill(0).map(v => new Array(target + 1).fill(false));
-
-  for (let i = 0; i < length; i++) {
-    dp[i][0] = true;
-  }
-  dp[0][nums[0]] = true;
-
-  for (let i = 1; i < length; i++) {
-    for (let j = 1; j <= target; j++) {
-      if (nums[i] <= j) {
-        i === 1 && j === 4 && console.log(dp[0][4]);
-        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
-        i === 1 && j === 4 && console.log(dp[0][4]);
-      } else {
-        dp[i][j] = dp[i - 1][j];
-      }
-    }
-  }
-  return dp[length - 1][target];
+  return result;
 };
-console.log(canPartition([2, 2, 3, 5]));
 
-var canPartition2 = function(nums) {
-  const n = nums.length;
-  if (n < 2) {
-      return false;
-  }
-  let sum = 0, maxNum = 0;
-  for (const num of nums) {
-      sum += num;
-      maxNum = maxNum > num ? maxNum : num;
-  }
-  if (sum & 1) {
-      return false;
-  }
-  const target = Math.floor(sum / 2);
-  if (maxNum > target) {
-      return false;
-  }
-  const dp = new Array(n).fill(0).map(v => new Array(target + 1).fill(false));
-  for (let i = 0; i < n; i++) {
-      dp[i][0] = true;
-  }
-  dp[0][nums[0]] = true;
-
-  for (let i = 1; i < n; i++) {
-      const num = nums[i];
-      for (let j = 1; j <= target; j++) {
-          if (j >= num) {
-              dp[i][j] = dp[i - 1][j] || dp[i - 1][j - num];
-          } else {
-              dp[i][j] = dp[i - 1][j];
-          }
-      }
-  }
-  return dp[n - 1][target];
-};
-// console.log(canPartition2([2, 2, 3, 5]));
+maxScore([1,2,3,4,5,6,1], 3);
